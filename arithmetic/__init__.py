b162007a40847c6a464ada228815980e529900f8
Szymon <szyszkaimyszka@gmail.com>
2025-10-28 22:52:14 +0100

    refactor: Reorganize as proper Python package

--- arithmetic/__init__.py
+++ arithmetic/__init__.py
@@ -1,4 +1,4 @@
-# arithmetic.py
+# arithmetic/__init__.py
 def add(a, b):
     # Requirement: return numeric sum of two numbers
     return a + b
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="arithmetic",
     version="0.1",
-    packages=find_packages(where="."),
-    package_dir={"": "."},
+    packages=find_packages(),
+    python_requires=">=3.6",
 )
